9ac066db9ecf481406445e18cf55c3cf4a020cec
tanandre <tanneman1@gmail.com>
2021-11-23 16:45:02 -0500

    add photoindex link

--- src/components/ProjectLinks.vue
+++ src/components/ProjectLinks.vue
@@ -3,7 +3,9 @@
     <p>Welcome to my home page. In case you are interested in one of my projects please click the links below.</p>
     <h3>My projects:</h3>
     <a v-for="link in links" :key="link.url" :href="link.url">
-      <div class="link">{{link.name}}</div>
+      <div class="link">{{link.name}}
+        <p>{{link.summary}}</p>
+      </div>
     </a>
   </div>
 </template>
@@ -12,30 +14,37 @@
 const links = [
   {
     name: 'Planning Poker (Vue+Socket.io)',
+    summary: 'Use with my team to do sprint planning. WebSockets are used to sync the data between the different browser sessions',
     url: 'http://planningpoker-bb.herokuapp.com/',
   },
   {
     name: 'Memori Game (Vue)',
+    summary: 'A simple memory game to entertain my kids with.',
     url: 'https://webmemori.netlify.app/',
   },
   {
-    name: 'Memori Game (Android)',
-    url: 'https://play.google.com/store/apps/details?id=nl.tan.memory',
+    name: 'PhotoIndex (Vue+PHP)',
+    summary: 'A Synology photo album app for personal use. Allows for easy searching/tagging of photos and using routing to determine state which results in proper backbutton support. Note the demo uses a mock back-end and only the query functionality works,',
+    url: 'https://photoindex-demo.netlify.app',
   },
   {
     name: 'String Tools (Vue)',
+    summary: 'Convenient String manipulation tools that I often use',
     url: 'https://stringtools.netlify.app/',
   },
   {
     name: 'Avatar Generator (Vue+SVG)',
+    summary: 'Creating random avatars for a game I want to build',
     url: 'https://avatar-generator.netlify.app/',
   },
   {
     name: 'Web XSLT Processor (React)',
+    summary: 'A tool to easily test XSLT scripts',
     url: 'https://webxslt.netlify.app/',
   },
   {
     name: 'Flight Tracker (Vue + GMaps)',
+    summary: 'Showing the current airplanes on a map using available open APIs',
     url: 'https://flight-tracker.netlify.app/',
   },
 ];
@@ -62,7 +71,7 @@ a {
 .links .link {
   margin: 2px;
   padding: 10px 25px;
-  width: 300px;
+  width: 500px;
   color: #fff;
   /* background-color: #61DAFB; */
   background-color: rgb(66, 131, 185);
